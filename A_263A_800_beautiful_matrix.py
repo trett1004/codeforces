794d98bdbf0bd8c2ba13bd3bfa38b3f516eba769
Max <maxtretter@tutanota.com>
2022-11-25 00:45:04 +0100

    accepted at codeforces

--- A_263A_800_beautiful_matrix.py
+++ A_263A_800_beautiful_matrix.py
@@ -2,6 +2,7 @@
 
 matrix = []
 
+# Following commenting  used for Testingpurpuses
 '''
 row1 = ['0', '0', '0', '0', '0']
 row2 = ['0', '0', '0', '0', '0']
@@ -16,29 +17,30 @@ matrix.append(row3)
 matrix.append(row4)
 matrix.append(row5)
 '''
-
+# Variable that contains the answer in the end
 stepcounter = 0
 
+# Get input
 for tc in range(5):
     line = input()
     # Make a Matrix
     row = []
     for j in range(len(line)):
         row.append(line[j])
+    # Remove empty spaces in the matrix
     row.pop(1)
     row.pop(2)
     row.pop(3)
     row.pop(4)
     matrix.append(row)
 
-#print(matrix[1][8])
-
+# Reduced linelength from 9 to 5 because of row.pop above
 newline = 5
 # iterate through the lines to find the "1"
 for i in range(5):
     for j in range(newline):
         if matrix[i][j] == "1":
-            #Check how many fields the 1 had to move and print result
+            #Check how many fields the 1 had to move to the middle and print result
             for p in range(i+1):
                 if i == 0 or i == 4:
                     stepcounter = stepcounter + 2
@@ -51,49 +53,10 @@ for i in range(5):
                 else:
                     stepcounter = stepcounter + 0
                 break
-                '''
-                if i == 0:
-                    stepcounter = stepcounter + 5
-                    stepcounter = newline - (j+1) + (3) + stepcounter
-                    break
-                elif i == 1:
-                    stepcounter = newline - (j+1) + (3)
-                    break
-                elif i == 2 and j < 2:
-                    stepcounter = 3 - (j+1)
-                    break
-                elif i == 2 and j == 2: # "1" is already in the middle
-                    break
-                elif i == 2 and j > 2:
-                    stepcounter = (4 - (j+2))
-                    break
-                elif i == 3:
-                    stepcounter = -(j) - (3)
-                    break
-                else:
-                    stepcounter = stepcounter - 5 - j - 3
-                    #stepcounter = -(newline) + (j+1) -(3) - stepcounter
-                    break
-                '''
             break
-
-#print("newline:", newline)
-#print("j+1:", j+1)
 print(stepcounter)
 
-#print("matrixij:", matrix[i][j])
-#print("matrix2,4:", matrix[2][2])
-#print("matrix1,8:", matrix[1][4])
 
-
-
-    # for j in range(len(line)):
-        #if line[j] == "1":
-            
-            
-            
-    # When "1" is found check for the position
-    # When found the position move the "1" to the middle wich is line 3 / column 3 and set the old place to "0"
 
 
 
